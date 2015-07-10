45971276f1ce393345ad810c481bb405337a40f7
alec harmon <alec.blumenfeld@mail.mcgill.ca>
2015-07-10 17:04:20 -0400

    entail code migration has begun --start screen xib

---
+++ TableDemo/AddViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddViewController.swift
+//  Food On Your Mind Shoping List
+//
+//  Created by alec on 7/10/15.
+//  Copyright (c) 2015 alec. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+}
